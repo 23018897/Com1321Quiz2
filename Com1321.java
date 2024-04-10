99097a47d92041e64f99f4fb416ebd866acff3d9
23018897 <163659526+23018897@users.noreply.github.com>
2024-04-10 21:43:03 +0200

    Add files via upload

---
+++ Com1321.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Com1321 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        // A = p(1+i/n)^n*m
+
+    System.out.print("Enter initial amount: ");
+    double p = input.nextDouble();
+
+    System.out.print("Enter Annual interest rate (in decimal): ");
+    double i = input.nextDouble();
+
+    System.out.print("Enter time period: ");
+    double n = input.nextDouble();
+
+    System.out.print("Enter interest coumpound mounthly : ");
+    double m = input.nextDouble();
+
+
+   double x = i/n;
+   double y = n*m;
+
+   double A1 = Math.pow((1+x), y);
+   double A2 = p*A1;
+
+   System.out.println("compound interest Amount is "+A2);
+
+   input.close(); 
+    }
+}
